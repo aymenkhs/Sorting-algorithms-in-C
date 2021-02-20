f6170745e7cdcdb278196179f0bf6ca14fe647a4
aymmenkhs <aymenkhouas@gmail.com>
2021-02-20 21:34:45 +0100

    project init

---
+++ table_creation.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "table_creation.h"
+
+long * generer_bon_ordre(long n){
+
+}
+
+long * generer_ordre_dinverse(long n){
+
+}
+
+long * generer_random(long n){
+
+}
---
+++ table_creation.h
@@ -0,0 +1,8 @@
+#ifndef table_creation
+#define table_creation
+
+long * generer_bon_ordre(long n);
+long * generer_ordre_dinverse(long n);
+long * generer_random(long n);
+
+#endif // table_creation
