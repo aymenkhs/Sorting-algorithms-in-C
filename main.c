034f21c854df5d141da2ebb8cae354a1ea1fed43
aymmenkhs <aymenkhouas@gmail.com>
2021-03-05 22:55:50 +0100

    init main with algos

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
 
 #include "functions.h"
 
+#include "algorithmes/insertion_sort.h"
+#include "algorithmes/bubble_sort.h"
+#include "algorithmes/merge_sort.h"
+#include "algorithmes/quick_sort.h"
+#include "algorithmes/heap_sort.h"
+
 int main(){
     return EXIT_SUCCESS;
 }
