906f6bf3a89e0b4c8414c873c4371b134efb9c59
Cher-Dj <63754209+Cherj@users.noreply.github.com>
2021-03-14 00:54:38 +0100

    Update merge_sort.c

--- algorithmes/merge_sort.c
+++ algorithmes/merge_sort.c
@@ -3,6 +3,56 @@
 
 #include "merge_sort.h"
 
-long * mergeSort(long * table, long taille){
+void Fusion(long * tab, long debut, long m, long fin)
+    {
+        long T2[fin-debut+1];
+        long i, j, k;
+        i = debut;
+        j = m+1;
+        k = 0;
+        if(debut != fin){
+        while (i <= m && j <= fin)
+        { if (tab[i] <= tab[j])
+            { T2[k] = tab[i];
+              i++;}
+          else{
+                T2[k] = tab[j];
+                j++;
+              }
+          k++;
+        }
+        // Copiez tous les éléments restants du tableau non vide
+        while (i <= m)
+        {
+            T2[k] = tab[i];
+			      i++;
+            k++;
+        }
+        
+        while (j <= fin)
+        {
+            T2[k] = tab[j];
+            j++;
+            k++;
+        }
+        k=0;
+		    for(int i=debut; i<=fin; i++)
+           {
+            tab[i]=T2[k];
+            k++;
+		       } 
+		        }
+    }
 
-}
+void Tri_fusion(long * T, long debut ,long fin)
+{ 
+  long m;
+  if(debut < fin)
+   { 
+     m=(debut+fin)/2;
+     Tri_fusion(T,debut,m);
+	   Tri_fusion(T,(m+1),fin);
+   	 Fusion(T,debut,m,fin);
+   } 
+ }
+
