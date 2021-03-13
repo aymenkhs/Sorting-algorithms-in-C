9eb2d02fea5f240771c0c9e54e477e59c6c57ec0
aymmenkhs <aymenkhouas@gmail.com>
2021-03-13 20:49:12 +0100

    HEAPSORT: some fixes

--- algorithmes/heap_sort.c
+++ algorithmes/heap_sort.c
@@ -4,23 +4,25 @@
 #include "heap_sort.h"
 
 long * heapSort(long * table, long taille){
-    long * heap = heapCreation(table, taille)
-    return heap
+    heapCreation(table, taille);
+    return table;
 }
 
 long * heapCreation(long * table, long taille){
     for(long i = (long) (taille-1)/2; i>=0; i--){
         table = entasser(table, taille, i);
     }
+    return table;
 }
 
 long * entasser(long * tab, long taille, long i){
     long imax = -1;
     long left_son = (2*i) + 1, right_son = (2*i) + 2;
-    if(left_son <= taille){ // in this case tab[i] isn't a leaf
-        if(right_son <= taille){ // we see if the tab[i] has a right child
+    printf("%d %d lft %d %d rght %d %d\n", i, tab[i], left_son, tab[left_son], right_son, tab[right_son]);
+    if(left_son < taille){ // in this case tab[i] isn't a leaf
+        if(right_son < taille){ // we see if the tab[i] has a right child
             // in this case the the node have a right and left child
-            if((tab[left_son] > tab[i]) || (tab[right_son] < tab[i])){
+            if((tab[left_son] > tab[i]) || (tab[right_son] > tab[i])){
                 if(tab[left_son] > tab[right_son]){
                     imax = left_son;
                 } else {
