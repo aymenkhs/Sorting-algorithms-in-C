bfb9502583c0a4267417f6355107f7af56761414
Cher-Dj <63754209+Cherj@users.noreply.github.com>
2021-03-14 00:49:18 +0100

    Update merge_sort.h

--- algorithmes/merge_sort.h
+++ algorithmes/merge_sort.h
@@ -1,6 +1,6 @@
 #ifndef merge
 #define merge
 
-long * mergeSort(long * table, long taille);
-
+void Fusion(long * tab, long debut, long m, long fin);
+void Tri_fusion(long * T, long debut ,long fin);
 #endif // merge
