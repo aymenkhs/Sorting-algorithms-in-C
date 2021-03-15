c7e6dd8cf258caa835ba596b73b8eba8917a6519
aymmenkhs <aymenkhouas@gmail.com>
2021-03-15 20:40:00 +0100

    MERGE SORT: fixing some stuff

--- algorithmes/merge_sort.c
+++ algorithmes/merge_sort.c
@@ -3,10 +3,21 @@
 
 #include "merge_sort.h"
 
+void mergeSort(long * table, long taille){
+    /*
+    cette fonction sert uniquement a appeller la fonction Tri_fusion avec es paramettres
+    table, 0 et taille
+    dans le but de garder un "main" plus propre
+    */
+    Tri_fusion(table, 0, taille);
+}
+
 void Fusion(long * tab, long debut, long m, long fin)
     {
-        long T2[fin-debut+1];
+        long *T2 = NULL;
         long i, j, k;
+
+        T2 = malloc((fin-debut+1) * sizeof(long));
         i = debut;
         j = m+1;
         k = 0;
@@ -27,7 +38,7 @@ void Fusion(long * tab, long debut, long m, long fin)
 			      i++;
             k++;
         }
-        
+
         while (j <= fin)
         {
             T2[k] = tab[j];
@@ -39,19 +50,18 @@ void Fusion(long * tab, long debut, long m, long fin)
            {
             tab[i]=T2[k];
             k++;
-           } 
-		        
+           }
+        free(T2);
     }
 
 void Tri_fusion(long * T, long debut ,long fin)
-{ 
+{
   long m;
   if(debut < fin)
-   { 
+   {
      m=(debut+fin)/2;
      Tri_fusion(T,debut,m);
 	   Tri_fusion(T,(m+1),fin);
    	 Fusion(T,debut,m,fin);
-   } 
+   }
  }
-
--- algorithmes/merge_sort.h
+++ algorithmes/merge_sort.h
@@ -1,6 +1,7 @@
 #ifndef merge
 #define merge
 
+void mergeSort(long * table, long taille);
 void Fusion(long * tab, long debut, long m, long fin);
 void Tri_fusion(long * T, long debut ,long fin);
 #endif // merge
