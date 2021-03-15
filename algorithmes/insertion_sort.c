9550478ea4cec85081c46544bab278a1354aba15
aymmenkhs <aymenkhouas@gmail.com>
2021-03-15 20:31:48 +0100

    INSERION SORT: Madjid's work

--- algorithmes/insertion_sort.c
+++ algorithmes/insertion_sort.c
@@ -3,6 +3,55 @@
 
 #include "insertion_sort.h"
 
-long * insertionSort(long * table, long taille){
+void insertionSort(long * table, long taille){
+    /*
+    cette fonction sert uniquement a appeller l'une des 2 fonctions (iterratif ou recurssive)
+    dans le but de garder un "main" plus propre
+    */
+    tri_insertion_iteratif(table, taille);
+}
+
+void tri_insertion_iteratif(long * t, long n)
+{
+
+	long i,j,Temp;
+
+	for(i=1;i<n;i++)
+	{
+		Temp = t[i];
+		j=i-1;
+
+		while(j>=0 && t[j]>Temp)
+		{
+			t[j+1] = t[j];
+			j--;
+		}
+
+		t[j+1] = Temp;
+	}
+
+}
+
+
+void tri_insertion_recursive(long *t,long i, long n)
+{
+
+	if(i<n)
+	{
+		long j,Temp;
+
+		Temp = t[i];
+		j=i-1;
+
+		while(j>=0 && t[j]>Temp)
+		{
+			t[j+1] = t[j];
+			j--;
+		}
+
+		t[j+1] = Temp;
+
+		tri_insertion_recursive(t,i+1,n);
+	}
 
 }
--- algorithmes/insertion_sort.h
+++ algorithmes/insertion_sort.h
@@ -1,6 +1,8 @@
 #ifndef insertion
 #define insertion
 
-long * insertionSort(long * table, long taille);
+void insertionSort(long * table, long taille);
+void tri_insertion_iteratif(long * t, long n);
+void tri_insertion_recursive(long *t,long i, long n);
 
 #endif // insertion
